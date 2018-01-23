d63967282f1fa8a49f156c1846bc31c13f5244d7
George the Master <sgazeos@gmail.com>
2018-01-23 21:04:30 +0200

    Fixed bugs with using putScalar and shape building

--- include/ops/declarable/generic/parity_ops/top_k.cpp
+++ include/ops/declarable/generic/parity_ops/top_k.cpp
@@ -10,33 +10,29 @@ namespace nd4j {
             NDArray<T>* x = INPUT_VARIABLE(0);
             int k = 1;// from params
             bool needSort = true;
-//            NDArray<T>* values = OUTPUT_VARIABLE(0);
-//            NDArray<int>* indeces = OUTPUT_VARIABLE(1);
-            if (block.getIArguments()->size() > 0) {
+            NDArray<T>* values = OUTPUT_VARIABLE(0);
+            NDArray<T>* indeces = OUTPUT_VARIABLE(1);
+            if (block.numI() > 0) {
                 k = INT_ARG(0);
                 needSort = INT_ARG(1);
             }
 
             if (k == 1) {
                 // using arg_max for it
-                NDArray<T>* values = OUTPUT_VARIABLE(0);
-                auto indeces = OUTPUT_VARIABLE(1);
-                //int index = 0;
-                //x->template applyTransform<nd4j::ops::argmax<T>>(indeces);
+
                 x->template applyIndexReduce<simdOps::IndexMax<T>>(indeces, {});
 
                 int index = indeces->getScalar(0);
                 T val = x->getScalar(index);
-                //OVERWRITE_2_RESULTS(vals, indeces);
                 
-                values->putScalar(val, 0);
-                //indeces->putScalar(index, 0);
+                values->putScalar(0, val);
+
                 return ND4J_STATUS_OK;
             }
             else if (k > 1) {
                 std::vector<int> inds(k);
                 std::vector<T> vals(k);
-                //indeces = x->template indexReduceNumber<simdOps::IndexMax<T>>(x->shape()->dimension());
+
                 for (int e = 0; e < k; e++) {
                     vals[e] = x->getScalar(e); // start initializing
                     inds[e] = e;
@@ -54,20 +50,20 @@ namespace nd4j {
                 }
             
                 // if need to be sort results
-            if (needSort) {
-                std::sort(inds.begin(), inds.end(), [vals](int a, int b) {
-                    return vals[a] > vals[b];   
-                });
-            }
-            auto vec_vals = new NDArray<T>('c', {1, k});
-            auto vec_idx = new NDArray<T>('c', {1, k});
+                if (needSort) {
+                    std::sort(inds.begin(), inds.end(), [vals](int a, int b) {
+                        return vals[a] > vals[b];   
+                    });
+                    std::sort(vals.begin(), vals.end(), [](int a, int b) {
+                        return a > b;   
+                    });
+                }
 
-            for (int e = 0; e < k; e++) {
-                vec_vals->putScalar(e, vals.at(e));
-                vec_idx->putScalar(e, inds.at(e));
-            }
-
-                OVERWRITE_2_RESULTS(vec_vals, vec_idx);
+                for (int e = 0; e < k; e++) {
+                    values->putScalar(e, vals.at(e));
+                    indeces->putScalar(e, inds.at(e));
+                }
+        
                 return ND4J_STATUS_OK;
             }
             else
@@ -79,7 +75,7 @@ namespace nd4j {
 
             int k = 1; // default output shape is size 1
 
-            if (block.getIArguments()->size() > 0) {
+            if (block.numI() > 0) {
                 k = INT_ARG(0);
             }
 
@@ -88,7 +84,7 @@ namespace nd4j {
                 ALLOCATE(newshape, block.getWorkspace(), shape::shapeInfoLength(k), int);
                 //std::vector<int> shape(k);
                 //{1, 1, k, 1, 1, 0, 1, 99};
-                shape::shapeBuffer(k, newshape);
+                ShapeBuilder::shapeVector(k, newshape);
                 shapeList->push_back(newshape); 
             }
             return shapeList;
