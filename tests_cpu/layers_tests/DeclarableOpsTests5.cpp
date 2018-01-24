4069664d1c72e7feba5af22195fe9c2d8a6b1e65
shugeo <sgazeos@gmail.com>
2018-01-24 18:44:49 +0200

    Changed test cases for both top_k and in_top_k operations

--- tests_cpu/layers_tests/DeclarableOpsTests5.cpp
+++ tests_cpu/layers_tests/DeclarableOpsTests5.cpp
@@ -382,3 +382,100 @@ TEST_F(DeclarableOpsTests5, eye_trest4) {
     delete results;
 }
 
+TEST_F(DeclarableOpsTests5, Test_TopK_1) {
+    NDArray<float> x('c', {2, 3}, {1.0f, 11.0f, 3.0f, 14.0f, 5.0f, 6.0f});
+    NDArray<float> expV('c', {2, 1}, {11.0f, 14.0f});
+    NDArray<float> expI('c', {2, 1}, {1.0f, 0.0f});
+
+    nd4j::ops::top_k<float> op;
+    auto result = op.execute({&x}, {}, {1, 0}); // without sorting
+
+    ASSERT_EQ(ND4J_STATUS_OK, result->status());
+
+    auto v = result->at(0);
+    auto i = result->at(1);
+
+    ASSERT_EQ(2, result->size());
+
+    ASSERT_TRUE(expV.isSameShape(v));
+    ASSERT_TRUE(expV.equalsTo(v));
+
+    ASSERT_TRUE(expI.isSameShape(i));
+    ASSERT_TRUE(expI.equalsTo(i));
+    
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests5, Test_TopK_2) {
+    NDArray<float> x('c', {2, 3, 4}, {11.0,  3.0, 14.0, 5.0, 
+                                       6.0,  9.0, 3.5, 7.0, 
+                                      21.0, 3.0, 14.0, 15.0, 
+                                       6.0, 9.0, 3.5, 7.0, 
+                                      11.0, 13.0, 14.0, 5.0, 
+                                      16.0, 9.0, 13.5, 7.0
+                                     }
+    );
+
+    NDArray<float> expV('c', {2, 3, 2}, {14.0f, 11.0f, 9.0f, 
+                                          7.0f, 21.0f, 15.0f,
+                                          9.0f, 7.0f, 14.0f, 
+                                         13.0f, 16.0f, 13.5f
+                                        }
+    );
+
+    NDArray<float> expI('c', {2, 3, 2 }, {2, 0, 1, 3, 0, 3, 1,  3, 2, 1, 0, 2});
+
+    nd4j::ops::top_k<float> op;
+    auto result = op.execute({&x}, {}, {2, 1});
+
+    ASSERT_EQ(ND4J_STATUS_OK, result->status());
+    ASSERT_EQ(2, result->size());
+    
+    auto v = result->at(0);
+    auto i = result->at(1);
+    
+    v->printShapeInfo("shape v");
+    expV.printShapeInfo("shape expV");
+
+    i->printShapeInfo("shape I");
+    expI.printShapeInfo("shape expI");
+
+    v->printIndexedBuffer("v");
+    expV.printIndexedBuffer("expV");
+    i->printIndexedBuffer("i");
+    expI.printIndexedBuffer("expI");
+    
+    ASSERT_TRUE(expV.isSameShape(v));
+    ASSERT_TRUE(expV.equalsTo(v));
+
+    ASSERT_TRUE(expI.isSameShape(i));
+    ASSERT_TRUE(expI.equalsTo(i));
+
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests5, Test_InTopK_1) {
+    NDArray<float> x('c', {2, 3}, {1.0, 11.0, 3.0, 14.0, 5.0, 6.0});
+    NDArray<float> y('c', {2}, {1, 1});
+    NDArray<float> expV('c', {2}, {1, 0});
+
+    nd4j::ops::in_top_k<float> op;
+    auto result = op.execute({&x, &y}, {}, {2});
+
+    ASSERT_EQ(ND4J_STATUS_OK, result->status());
+    ASSERT_EQ(1, result->size());
+    
+    auto v = result->at(0);
+    
+    v->printShapeInfo("InTopK: shape v");
+    expV.printShapeInfo("InTopK: shape expV");
+
+    v->printIndexedBuffer("v");
+    expV.printIndexedBuffer("expV");
+    
+    ASSERT_TRUE(expV.isSameShape(v));
+    ASSERT_TRUE(expV.equalsTo(v));
+
+    delete result;
+}
+
