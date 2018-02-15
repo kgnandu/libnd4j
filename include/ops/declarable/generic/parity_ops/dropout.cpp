f588bfed99b1f3e23f3b62052fc2ab0fe9982b56
shugeo <sgazeos@gmail.com>
2018-02-15 10:35:05 +0200

    dropout op final.

--- include/ops/declarable/generic/parity_ops/dropout.cpp
+++ include/ops/declarable/generic/parity_ops/dropout.cpp
@@ -47,80 +47,44 @@ CONFIGURABLE_OP_IMPL(dropout, 1, 1, true, 1, 1) {
 
 
 
-//    Nd4jIndex tensorCount = x.tensorsAlongDimension({1});
-//    nd4j_printf("Total subarray are %i\n", tensorCount);
     if (reduceShape == nullptr)
         input->template applyRandom<randomOps::DropOutInverted<T>>(rng, nullptr, output, &probValue);
     else {
-    REQUIRE_TRUE(reduceShape->lengthOf() <= input->rankOf(), 0, "dropout: Noise shape should be fittable to input");
-
-    std::vector<int> dims(reduceShape->lengthOf());
-
-    bool fit = true;
-
-    for( int i = 0; i < dims.size(); i++ ) {
-        dims[i] = (*reduceShape)(i);
-        for (int e = 0; e < input->rankOf(); ++e)
-            if (input->sizeAt(e) % dims[i]) {
-                fit = false;
-                break;
-            }
-
-        if(!fit) break;
-    }
-
-    // check dims to fit input
-    REQUIRE_TRUE(fit, 0, "dropout: Noise shape should fit to input rank.");
-    NDArray<T> chunk('c', dims);
-    chunk.assign(T(1.0));
-    chunk.template applyRandom<randomOps::DropOutInverted<T>>(rng, nullptr, &chunk, &probValue);
-
-    // broadcast chunk to full matrix
-    NDArray<T>* multiplier = new NDArray<T>(*input);
-    chunk.printIndexedBuffer("Chunk is ");
-    multiplier->assign(T(0.0));
-    //chunk.repeat(0, *multiplier);
-    *multiplier += chunk;
-//    int k = 0;
-//    for (int e = 0; e < multiplier->lengthOf(); ++e) {
-//        (*multiplier)(e) = chunk(k++);
-//        if (k >= chunk.lengthOf()) k = 0;
-//    }
-    multiplier->printIndexedBuffer("Multiplier is");
-    input->template applyPairwiseTransform<simdOps::Multiply<T>>(multiplier, output, nullptr);
-    delete multiplier;
+        REQUIRE_TRUE(reduceShape->lengthOf() <= input->rankOf(), 0, "dropout: Noise shape should be fittable to input");
+    
+        std::vector<int> dims(reduceShape->lengthOf());
+    
+        bool fit = true;
+    
+        for( int i = 0; i < dims.size(); i++ ) {
+            dims[i] = (*reduceShape)(i);
+            for (int e = 0; e < input->rankOf(); ++e)
+                if (input->sizeAt(e) % dims[i]) {
+                    fit = false;
+                    break;
+                }
+    
+            if(!fit) break;
+        }
+    
+        // check dims to fit input
+        REQUIRE_TRUE(fit, 0, "dropout: Noise shape should fit to input rank.");
+        NDArray<T> chunk('c', dims);
+        chunk.assign(T(1.0));
+        chunk.template applyRandom<randomOps::DropOutInverted<T>>(rng, nullptr, &chunk, &probValue);
+    
+        // broadcast chunk to full matrix
+        std::unique_ptr<NDArray<T>> dropOutMultiplier(new NDArray<T>(*input));
+        dropOutMultiplier->assign(T(0.0));
+    
+        *dropOutMultiplier += chunk;
+    
+        input->template applyPairwiseTransform<simdOps::Multiply<T>>(dropOutMultiplier.get(), output, nullptr);
     }
     nativeOps.destroyRandom(rng);
 
     return ND4J_STATUS_OK;
 }
-/*
-DECLARE_SHAPE_FN(embedding_lookup) {
-
-    int* inShapeInfo = inputShape->at(0);
-    int* indecesShapeInfo = inputShape->at(1);
-    int inRank = shape::rank(inShapeInfo);
-
-    int outRank = inRank; 
-
-    int* outShapeInfo = nullptr;
-    
-    ALLOCATE(outShapeInfo, block.getWorkspace(), shape::shapeInfoLength(outRank), int);
-    std::vector<int> shapeInfo(outRank);
-
-    shapeInfo[0] = indecesShapeInfo[1]; // vector - how many elements
-    for (int e = 1; e < outRank; e++)
-        shapeInfo[e] = shape::sizeAt(inShapeInfo, e);
-    if (shape::order(inShapeInfo) == 'c')
-        shape::shapeBuffer(outRank, shapeInfo.data(),  outShapeInfo);
-    else
-        shape::shapeBufferFortran(outRank, shapeInfo.data(),  outShapeInfo);
-
-    return new ShapeList(outShapeInfo);    
-}
-
-*/
-
 
 }
 }
